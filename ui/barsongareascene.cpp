fe55539f76d9c538e24cbd8e6ca718028ec7219a
jeyroi <jean.eyroi@ecl2013.ec-lyon.fr>
2013-03-22 13:24:37 +0100

    Documented class BARSongAreaScene.

--- ui/barsongareascene.cpp
+++ ui/barsongareascene.cpp
@@ -4,66 +4,71 @@
 #include <QDebug>
 #include <QMimeData>
 #include <QPointF>
-#
 
-BARSongAreaScene::BARSongAreaScene(QObject *parent) :
-    QGraphicsScene(parent)
+/**
+ * @brief Scene of the timeline.
+ */
+
+/**
+ * @brief Constructor.
+ * @param[in] parent widget.
+ */
+BARSongAreaScene::BARSongAreaScene(QObject *parent) : QGraphicsScene(parent)
 {
-    timeLineSize=100;
-    QBrush redBrush(Qt::white);
-    QPen blackPen(Qt::black);
+    timeLineSize=100; /**< length of each rectangle composing the timeline. */
 
-    blackPen.setWidth(1);
-    for(int i=0;i<50;i++)
+    QBrush redBrush(Qt::white); /**< desired color for the background of the rectangles when empty. */
+    QPen blackPen(Qt::black); /**< desired color for the outline of the rectangle. */
+    blackPen.setWidth(1); /**< sets width of the outline of the rectangle. */
+
+    for(int i=0;i<50;i++) /**< this boucle creates the 50 rectangles of the timeline. */
     {
-        QGraphicsRectItem *rectangle=new QGraphicsRectItem(i*timeLineSize,0,timeLineSize,60);
-        QGraphicsTextItem *label=new QGraphicsTextItem(QString::number(i));
-        label->setPos(i*timeLineSize-timeLineSize/20,-25);
-                rectangle->setBrush(redBrush);
-                rectangle->setPen(blackPen);
-                addItem(rectangle);
-                addItem(label);
-                stock.push_back(rectangle);
+        QGraphicsRectItem *rectangle=new QGraphicsRectItem(i*timeLineSize,0,timeLineSize,60); /**< create the new rectangle on the right of the last one. */
+        QGraphicsTextItem *label=new QGraphicsTextItem(QString::number(i)); /**< displays the number of the rectangle in the timeline. */
+        label->setPos(i*timeLineSize-timeLineSize/20,-25); /**< position the label in the top left corner of the rectangle. */
+        rectangle->setBrush(redBrush); /**< set the background color. */
+        rectangle->setPen(blackPen); /**< set the outline. */
+        addItem(rectangle); /**< the last three lines add the rectangle to the timeline. */
+        addItem(label);
+        stock.push_back(rectangle);
     }
-
 }
 
+/**
+ * @brief Implements the drop of a pattern on the timeline.
+ * The drag-and-dropped pattern is inserted in the pointed rectangle of the timeline.
+ * @param[in] drop event.
+ */
 void BARSongAreaScene::dropEvent(QGraphicsSceneDragDropEvent *event)
 {
-    qDebug()<<"DROOOOP à "<<event->scenePos().x()<<" et à "<<event->scenePos().y()<<endl;
-
-
-    QColor currentColor;
+    QColor currentColor; /**< creation of the two variables that will receive the information sent by through the drag-and-drop. */
     int patternLength;
-    QByteArray itemData= event->mimeData()->data("application/x-dnditemdata");
+
+    QByteArray itemData= event->mimeData()->data("application/x-dnditemdata"); /**< unpack the information stored in the MIME file. */
     QDataStream droppedData(&itemData, QIODevice::ReadOnly);
-    droppedData>>currentColor;
+
+    droppedData>>currentColor; /**< store the unpacked information in each relevant variable. */
     droppedData>>patternLength;
 
-
-    qDebug() << event->scenePos().toPoint().x() << endl;
-    QGraphicsRectItem *testdrop=new QGraphicsRectItem(event->scenePos().toPoint().x() - (event->scenePos().toPoint().x() % timeLineSize),0,timeLineSize*patternLength,60);
-    QBrush currentBrush(currentColor);
+    QGraphicsRectItem *testdrop=new QGraphicsRectItem(event->scenePos().toPoint().x() - (event->scenePos().toPoint().x() % timeLineSize),0,timeLineSize*patternLength,60); /**< creates a rectangle in the rectangle of the timeline where the drop occured. */
+    QBrush currentBrush(currentColor); /**< sets the color of the rectangle. This color corresponds to the color of the pattern that was dragged-and-dropped. */
     testdrop->setBrush(currentBrush);
 
-    //event->mimeData()->data()
-
-
-
     addItem(testdrop);
-
-
-
 }
 
+/**
+ * @brief Give clearance for the drop.
+ * @param[in] drag enter event
+ */
 void BARSongAreaScene::dragEnterEvent(QGraphicsSceneDragDropEvent *event)
 {
     event->setAccepted(true);
-    qDebug()<<"DRAG EN COUUUURS !"<<endl;
     event->acceptProposedAction();
 }
 
-void BARSongAreaScene::dragMoveEvent(QGraphicsSceneDragDropEvent *event)
-{
-    qDebug()<<"Ça bouuuuuge ! "<<event->scenePos().x()<<endl;
-}
+/**
+ * @brief Required for the drop to happen (don't know exactly why, found on the Internet).
+ * @param[in] drag enter event
+ */
+void BARSongAreaScene::dragMoveEvent(QGraphicsSceneDragDropEvent *event){}
--- ui/barsongareascene.h
+++ ui/barsongareascene.h
@@ -6,19 +6,20 @@
 #include <QGraphicsRectItem>
 #include <QDropEvent>
 #include <QGraphicsSceneDragDropEvent>
+
+/**
+ * @brief Scene of the timeline.
+ */
+
 class BARSongAreaScene : public QGraphicsScene
 {
     Q_OBJECT
 public:
     explicit BARSongAreaScene(QObject *parent = 0);
-    
-signals:
-    
-public slots:
 
 private:
-    QVector<QGraphicsRectItem*> stock;
-    int timeLineSize;
+    QVector<QGraphicsRectItem*> stock; /**< the timeline is composed of rectangles. This vectors contains is the list of the rectangles of the timeline. */
+    int timeLineSize; /**< length of each rectangle composing the timeline. */
 
 protected:
     void dropEvent(QGraphicsSceneDragDropEvent *event);
