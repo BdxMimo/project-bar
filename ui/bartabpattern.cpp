7f49a68a0298c420a2e3c98979224acce65f76fa
jeyroi <jean.eyroi@ecl2013.ec-lyon.fr>
2013-03-22 11:34:34 +0100

    Documented class BARTabPattern.

--- ui/bartabpattern.cpp
+++ ui/bartabpattern.cpp
@@ -5,6 +5,14 @@
 #include <QFileDialog>
 #include <QFileInfo>>
 
+/**
+ * @brief Implements the interface in the "pattern" tab.
+ */
+
+/**
+ * @brief Standard constructor.
+ * @param[in] parent widget.
+ */
 BARtabPattern::BARtabPattern(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::BARtabPattern)
@@ -12,22 +20,26 @@ BARtabPattern::BARtabPattern(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief Default destructor.
+ */
 BARtabPattern::~BARtabPattern()
 {
     delete ui;
 }
 
+/**
+ * @brief Slot that triggers the creation of a new sound bar.
+ */
 void BARtabPattern::on_buttonAddSoundBar_clicked()
 {
-    QString path = QFileDialog::getOpenFileName(this, tr("Open File"), tr("Files(*.*)"));
-    QFileInfo fi(path);
-    QString name=fi.baseName();
+    QString path = QFileDialog::getOpenFileName(this, tr("Open File"), tr("Files(*.*)")); /**< open dialog box. The user selects the sound he wants to add, and the path is retrieved. */
+    QFileInfo fi(path); /**< retrieve the full path to the file. */
+    QString name=fi.baseName(); /**< extract the name of the sound from the full path. */
 
-
-    // test if user didn't cancel in the file explorer : avoid the creation of a new soundbar with no file
-    if(name!="")
+    if(name!="") /**< check if the path is not void, in case the user canceled the transaction in the dialog box or selected no file. */
     {
-        BARSoundBar *newBarSoundBar=new BARSoundBar(this,name);
-        ui->soundBarArea->addWidget(newBarSoundBar);
+        BARSoundBar *newBarSoundBar=new BARSoundBar(this,name); /**< creates the new sound bar and provides the name of the sound associated to the new soundbar to the constructor. */
+        ui->soundBarArea->addWidget(newBarSoundBar); /** adds the new sound bar at the bottom of the layout, under the last sound bar and over the "+" button. */
     }
 }
